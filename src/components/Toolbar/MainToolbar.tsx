19955eabcc88bbf5febc62bd69cb6c409ea32aed
Evan Durand Arranz Herranz <evan.durand-arranz-herranz@epitech.eu>
2026-05-05 10:09:44 +0200

    ADD - save/save as + readme

--- src/components/Toolbar/MainToolbar.tsx
+++ src/components/Toolbar/MainToolbar.tsx
@@ -50,7 +50,7 @@ function AGRLogo() {
         <p className="text-white font-bold text-sm leading-tight" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
           Tactical Board
         </p>
-        <p className="text-gray-500 text-xs leading-tight">v1.8</p>
+        <p className="text-gray-500 text-xs leading-tight">v1.9</p>
       </div>
     </div>
   )
