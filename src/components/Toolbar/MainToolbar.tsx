2dcbf79d7aad663b8c5fc7dd4292f98a517f69cb
Evan Durand Arranz Herranz <evan.durand-arranz-herranz@epitech.eu>
2026-05-06 17:59:02 +0200

    TAG - new

--- src/components/Toolbar/MainToolbar.tsx
+++ src/components/Toolbar/MainToolbar.tsx
@@ -53,7 +53,7 @@ function AGRLogo() {
         <p className="text-white font-bold text-sm leading-tight" style={{ fontFamily: 'Rajdhani, sans-serif' }}>
           Tactical Board
         </p>
-        <p className="text-gray-500 text-xs leading-tight">v1.9</p>
+        <p className="text-gray-500 text-xs leading-tight">v1.10</p>
       </div>
     </div>
   )
